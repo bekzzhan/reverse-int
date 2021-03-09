f10648baebc3dc28889ab80de4797cf511fa0c1c
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2021-03-09 18:22:14 +0600

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/* jshint esversion: 6 */
 module.exports = function reverse (n) {
-  
-}
+  const str = String(n);
+  const arr = [];
+  for (const item of str) {
+    arr.push(item);
+  }
+  return Number.parseInt(arr.reverse().join(''));
+};
